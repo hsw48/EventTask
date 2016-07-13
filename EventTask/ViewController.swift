335d41fcfc57dfd4bd8e80a1436aa393834cf9d0
Harrison Woodward <harrisonwoodward48@gmail.com>
2016-07-13 11:53:40 -0700

    Initial Commit

---
+++ EventTask/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EventTask
+//
+//  Created by Julia Woodward on 7/13/16.
+//  Copyright © 2016 Harrison Woodward. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
